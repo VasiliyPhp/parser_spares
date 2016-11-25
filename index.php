ece1a8d77b990cd2561caf0769bbbfcbedd56949
SGA-SERVER <SGA-SERVER>
2016-11-25 15:32:15 +0700

    add my project second

--- index.php
+++ index.php
@@ -12,10 +12,10 @@ foreach($cats as $cat){
 }
 
 function get_main_categories(){
-	$tmp = file_get_contents(SITE);
-	$document = phpQuery::newDocument($tmp);
 	$cats = get('cats');
 	if(!$cats){
+		$tmp = file_get_contents(SITE);
+		$document = phpQuery::newDocument($tmp);
 		$i = 0;
 		foreach(pq('#lightCars a') as $item){
 			
@@ -25,12 +25,45 @@ function get_main_categories(){
 		}
 		set('cats', $cats);
 	}
+	phpQuery::unloadDocuments();
 	return ($cats) ? : [];
 }
 
 function parse($cat){
 	$marka = $cat['title'];
 	$models = file_get_contents(SITE . $cat['href']);
-	$allModels = [];
+	$models = phpQuery::newDocument($models);
+	$models = pq('.model-list a');
+	foreach($models as $model){
+		
+		$img   = pq($model)->find('img')->attr('src');
+		$_model = trim(pq($model)->find('.item-model-info')->text());
+		$href  = trim(pq($model)->attr('href'));
+		
+		save_img($marka, $_model, $img);
+		if(!$href){
+			continue;
+		}
+		phpQuery::unloadDocuments();
+		
+		$submodelsDoc = file_get_contents(SITE . $href);
+		
+		$submodelsDoc = phpQuery::newDocument($submodelsDoc)->find('.model-list a');
+		echo $submodelsDoc; exit;
+		
+		
+		
+	}
+	
+	echo $models;
 	j($models);
+}
+
+
+function save_img($marka, $model, $img){
+	
+	$path = "imgs/$marka/$model/";
+	file_exists($path) || mkdir($path, null, 1);
+	file_exists($path . 'auto.jpg')|| file_put_contents($path . 'auto.jpg', file_get_contents($img));
+	
 }
