e86506a83463dccd334781111d6da02eee12b7b5
Vasiliy Gerlack <mister.sergeew-v@yandex.ru>
2016-12-02 02:48:07 +0700

    dodelal

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+exec('chcp 65001');
+
 $allCats = [
 	'Audi',
 	'Chery',
@@ -28,15 +30,13 @@ $allCats = [
 ];
 const SITE = 'http://euroauto.ru';
 
-if(isset($_POST['cats'])){
 	require 'phpQuery/phpQuery.php';
 	require 'helper_functions.php';
 	require 'core.php';
 	set_time_limit(-1);
 	touch('checker.dd');
 	// remove('cats');
-	$needle = $_POST['cats'];
-	id($_POST['id']);
+	$needle = $allCats;
 	$cats = get_main_categories();
 
 	foreach($cats as $cat){
@@ -46,14 +46,4 @@ if(isset($_POST['cats'])){
 		}
 		parse($cat);
 	}
-}else{
-	echo "<form method=post target='_blank' >";
-	foreach($allCats as $cat ){
-		echo '<label><input name="cats[]" type=checkbox value="'.$cat.'" > '.$cat.'</label><br/>';
-	}
-	echo '<label> начальный id <input name=id  type=number /></label><br/>';
-	echo '<input type=submit value=start />';
-	echo '</form>';
-	echo '<a href="stop.php" >stop</a></br>';
-	echo '<a onclick="return confirm(\'Are you shure\');" href="clear.php" >clear</a>';
-}
+
